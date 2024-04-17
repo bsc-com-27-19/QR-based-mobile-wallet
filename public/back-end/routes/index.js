da7959f142b7410f46b8beb4a250d20aeff76989
bsc-com-05-19 <bsc-com-05-19@unima.ac.mw>
2024-04-17 11:50:46 +0200

    updated version all routes

--- public/back-end/routes/index.js
+++ public/back-end/routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const { getAllUsers } = require('../controllers/userController');
 
+
+router.get('/users', getAllUsers);
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/protected', authController.protected);
