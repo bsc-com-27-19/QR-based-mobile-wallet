7d5c5ffdb48e4fe8db78ac4f9ffc4657f5629fa1
bsc-com-27-19 <bsc-com-27-19@unima.ac.mw>
2024-06-13 20:59:54 +0200

    Modified some files

--- public/back-end/db.js
+++ public/back-end/db.js
@@ -1,10 +1,16 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "izepay_x9n8_user",
-  host: "dpg-cpl9efn109ks73dofj1g-a",
-  database: "izepay_x9n8",
-  password: "1oNyWd4iFwvnfzy4W09lxM3sFdeppeBT",
+  // user: "izepay_x9n8_user",
+  // host: "dpg-cpl9efn109ks73dofj1g-a",
+  // database: "izepay_x9n8",
+  // password: "1oNyWd4iFwvnfzy4W09lxM3sFdeppeBT",
+  // port: 5432,
+
+  user: "postgres",
+  host: "localhost",
+  database: "izepay",
+  password: "0002",
   port: 5432,
 });
 
