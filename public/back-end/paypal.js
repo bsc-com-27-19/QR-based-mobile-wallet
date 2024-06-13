8e62b828fe40c5ea7eed2c2a7bb3f3fb0ecc4715
Flonicah Phiri <bsc-com-27-19@unima.ac.mw>
2024-06-13 21:29:59 +0300

    implemented notification functionality

--- public/back-end/paypal.js
+++ public/back-end/paypal.js
@@ -1,8 +1,24 @@
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
 const pool = require('./db');
+const { Vonage } = require('@vonage/server-sdk');
 
 const BASE_URL = 'https://api-m.sandbox.paypal.com'; // Use sandbox environment for testing
+
+// Initialize Vonage client
+const vonage = new Vonage({
+    apiKey: "87b97ea9",
+    apiSecret: "HDpUYXWST0xBvQpl"
+});
+const from = "IZEpay";
+const to = "265997189926";
+
+// Function to send SMS
+async function sendSMS(text) {
+    await vonage.sms.send({ to, from, text })
+        .then(resp => { console.log('Message sent successfully'); console.log(resp); })
+        .catch(err => { console.log('There was an error sending the messages.'); console.error(err); });
+}
 
 // Function to obtain an OAuth2 access token from PayPal for a specific user
 async function getAccessToken(clientId, clientSecret) {
@@ -66,6 +82,17 @@ async function createAndCaptureOrder(purchaseUnits, card, clientId, clientSecret
         if (captureResponse.data.status !== 'COMPLETED') {
             throw new Error('Failed to capture payment');
         }
+        const payerName = captureResponse.data.payment_source.card.name;
+        const amount = captureResponse.data.purchase_units[0].payments.captures[0].amount.value;
+        const currency = "MWK";
+        const transactionId = captureResponse.data.purchase_units[0].payments.captures[0].id;
+        const date = new Date().toLocaleString();
+
+        // Construct the SMS text with payment details
+        const text = `Transaction ID: ${transactionId}\nYou have successfully received an amount of ${currency} ${amount} from ${payerName}\nDate: ${date}\n\nThank you for using IZEpay.\n\n`;
+
+        // Send SMS upon successful payment capture
+        await sendSMS(text);
 
         return captureResponse.data;
     } catch (error) {
